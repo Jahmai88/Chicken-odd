4d3a734c0d4ec21fb2e6b39aab61097fdc71024b
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 09:52:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = 15;
+        if (num > 10) {
+        System.out.println("You win.");}//endif
+        if (num < 10) {
+            System.out.println("You lose.");}//endif
+        }
+    }
+
